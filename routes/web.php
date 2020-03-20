dab7e7034e8393162bdb53564af1fe592bac5045
NetHunter <ranggaaprilliio@gmail.com>
2020-03-21 00:15:05 +0700

    JWT LUMEN UPDATE

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,26 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+Route::group([
+
+    // 'middleware' => 'api',
+    'prefix' => 'auth'
+  
+  ], function ($router) {
+    Route::post('register', 'AuthController@register');
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+  
+  });
